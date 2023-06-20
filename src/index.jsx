3fb2fa8c7bdc7d744faf2de9fc4284075b43d3be
Christopher Allen <christopher.allen@assetworks.com>
2023-06-20 10:47:34 -0700

    Add queue count to console

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,20 @@
-import ForgeUI, { render, QueuePage, Fragment, Text } from '@forge/ui';
+import ForgeUI, { render, QueuePage, Fragment, Text, useProductContext, useState } from '@forge/ui';
+import api, { route } from '@forge/api'
+
+const getJsmQueues = async (serviceDeskKey) => {
+    const res = await api
+        .asUser()
+        .requestJira(
+            route`/rest/servicedeskapi/servicedesk/${serviceDeskKey}/queue`
+        );
+    const data = await res.json();
+    return data.values;
+};
 
 const App = () => {
+    const context = useProductContext();
+    const [queues] = useState(async () => await getJsmQueues(context.extensionContext.project.key));
+    console.log(`Number of queues: ${queues.length}`);
     return (
         <Fragment>
             <Text>Hello world!</Text>
